7d19eca666257eeba9a82071ed0a594570d4868a
dlzwtk <2020694283@qq.com>
2025-09-04 17:16:26 +0800

    ch3 finished

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -38,8 +38,34 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     0
 }
 
-// TODO: implement the syscall
+///handle systrace
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
-    trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 => {
+            let id_ptr = _id as *const u8; // 将 id 视作指针
+            unsafe {
+                // 读取指针地址处的值
+                *id_ptr as isize
+            }
+        }
+
+        1 => {
+            let id_ptr = _id as *mut u8; // 将 id 视作可变指针
+            unsafe {
+                // 写入 data 的最低字节到指针地址处
+                *id_ptr = _data as u8;
+            }
+            0
+        }
+
+        2 => {
+            use crate::task::get_current_syscall_count; // 导入获取系统调用计数的函数
+            let syscall_counts = get_current_syscall_count();
+            syscall_counts[_id] as isize // 返回编号为 id 的系统调用次数
+        }
+
+        _ => {
+            panic!("Unsupported trace_request: {}", _trace_request);
+        }
+    }
 }
--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -15,6 +15,7 @@ mod switch;
 mod task;
 
 use crate::config::MAX_APP_NUM;
+use crate::config::MAX_SYSCALL_NUM;
 use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
 use lazy_static::*;
@@ -54,6 +55,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            syscall_cnts: [0;MAX_SYSCALL_NUM],
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -135,6 +137,20 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+
+    //count syscall
+    fn count_syscall(&self, syscall_id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_cnts[syscall_id] += 1;
+    }
+
+    //get syscall count
+    fn get_syscall_count(&self) -> [u32; MAX_SYSCALL_NUM] {
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_cnts
+    }
 }
 
 /// Run the first task in task list.
@@ -168,4 +184,14 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+
+/// Count the syscall of current 'Running' task
+pub fn count_current_syscall(syscall_id: usize) {
+    TASK_MANAGER.count_syscall(syscall_id);
+}
+
+/// Get the syscall count of current 'Running' task
+pub fn get_current_syscall_count() -> [u32; MAX_SYSCALL_NUM] {
+    TASK_MANAGER.get_syscall_count()
 }
